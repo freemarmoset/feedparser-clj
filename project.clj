44bffe01188a56e063f90ef3f657beecbce40fbf
Joe Banks <joe@upcity.com>
2015-07-23 14:26:10 -0500

    update to use recent(and patched) version of Rome

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.scsibug/feedparser-clj "0.5.0"
+(defproject org.clojars.freemarmoset/feedparser-clj "0.6.0"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.6.0"]
 		 [org.jdom/jdom "2.0.2"]
-		 [net.java.dev.rome/rome "1.0.0"]]
+		 [org.clojars.freemarmoset/rome "1.6.0"]]
   :main feedparser-clj.core)
