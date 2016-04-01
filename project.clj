f67be2b728e526bf24bada7ad842f3a3e7b5409f
Joe Banks <joe@upcity.com>
2016-04-01 10:46:30 -0500

    bump version of rome tools

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject org.clojars.freemarmoset/feedparser-clj "0.6.0"
+(defproject org.clojars.freemarmoset/feedparser-clj "0.6.1"
   :description "Parse RSS/Atom feeds with a simple, clojure-friendly API."
   :dependencies [[org.clojure/clojure "1.6.0"]
 		 [org.jdom/jdom "2.0.2"]
-		 [org.clojars.freemarmoset/rome "1.6.0"]]
-  :main feedparser-clj.core)
+		 [com.rometools/rome "1.6.0"]]
+  :main feedparser-clj.core
+  :deploy-repositories {"clojars-https" {:url "https://clojars.org/repo"
+                                          :sign-releases false}})
